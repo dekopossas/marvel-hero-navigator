a4b099a1cc9aec81ec83ee9529ab99fc15774312
deko possas <andrepossaster@gmail.com>
2021-04-08 20:38:57 -0300

    adicionei o react router don

--- marvel-hero-navegator-frontend/src/App.jsx
+++ marvel-hero-navegator-frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       marvel hero navegator
-    </div>
+    </BrowserRouter>
   );
 }
 
