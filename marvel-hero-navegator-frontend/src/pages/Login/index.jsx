bd98e1cb0d6cbdabccf77725ba4c03ae22cf5a61
deko possas <andrepossaster@gmail.com>
2021-04-09 15:09:58 -0300

    tirei o nodemodelus do lugar errado

--- marvel-hero-navegator-frontend/src/pages/Login/index.jsx
+++ marvel-hero-navegator-frontend/src/pages/Login/index.jsx
@@ -3,42 +3,40 @@ import S from './style';
 
 function Login() {
   return (
-    <S.Container>
-      <div className="text-center">
-        <form className="form-signin">
-          <h1 className="h1 mb-3 font-weight-normal">Login</h1>
-          <label htmlFor="email">
-            Email
-            <input className="form-control" type="email" id="email" data-testid="email-input" />
-          </label>
-          <label htmlFor="password">
-            Senha
-            <input
-              className="form-control"
-              type="password"
-              id="password"
-              data-testid="password-input"
-            />
-          </label>
-          <button
-            className="btn btn-lg btn-danger btn-block"
-            type="button"
-            data-testid="signin-btn"
-          >
-            Entrar
-          </button>
-          <br />
-          <button
-            to="/register"
-            data-testid="no-account-btn"
-            className="btn btn-lg btn-primary"
-            type="button"
-          >
-            Register
-          </button>
-          <p className="mt-5 mb-3 text-muted">© Marvel Hero Navegator 2021</p>
-        </form>
-      </div>
+    <S.Container className="text-center">
+      <S.Form className="form-signin">
+        <h1 className="h1 mb-3 font-weight-normal">Login</h1>
+        <label htmlFor="email">
+          Email
+          <input className="form-control" type="email" id="email" data-testid="email-input" />
+        </label>
+        <label htmlFor="password">
+          Senha
+          <input
+            className="form-control"
+            type="password"
+            id="password"
+            data-testid="password-input"
+          />
+        </label>
+        <button
+          className="btn btn-lg btn-danger btn-block"
+          type="button"
+          data-testid="signin-btn"
+        >
+          Entrar
+        </button>
+        <br />
+        <button
+          to="/register"
+          data-testid="no-account-btn"
+          className="btn btn-lg btn-primary"
+          type="button"
+        >
+          Register
+        </button>
+        <p className="mt-5 mb-3 text-muted">© Marvel Hero Navegator 2021</p>
+      </S.Form>
     </S.Container>
   );
 }
--- marvel-hero-navegator-frontend/src/pages/Login/style.js
+++ marvel-hero-navegator-frontend/src/pages/Login/style.js
@@ -1,7 +1,31 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  width: 100%;
+  display: flex;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #f5f5f5;
+  height: 100%;
 `;
 
-export default { Container };
+const Form = styled.form`
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+`;
+
+const Input = styled.input`
+  position: relative;
+  box-sizing: border-box;
+  height: auto;
+  padding: 10px;
+  font-size: 16px;
+`;
+
+export default {
+  Container,
+  Form,
+  Input,
+};
