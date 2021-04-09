e5d487e434c500d603d2e250fd16f9326804d481
deko possas <andrepossaster@gmail.com>
2021-04-09 15:30:31 -0300

    tirei styled component

--- marvel-hero-navegator-frontend/src/pages/Login/index.jsx
+++ marvel-hero-navegator-frontend/src/pages/Login/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import S from './style';
+import './login.css';
 
 function Login() {
   return (
-    <S.Container className="text-center">
-      <S.Form className="form-signin">
+    <div className="text-center">
+      <form className="form-signin">
         <h1 className="h1 mb-3 font-weight-normal">Login</h1>
         <label htmlFor="email">
-          Email
-          <input className="form-control" type="email" id="email" data-testid="email-input" />
+          <input
+            className="form-control"
+            type="email"
+            id="email"
+            data-testid="email-input"
+          />
         </label>
         <label htmlFor="password">
-          Senha
           <input
             className="form-control"
             type="password"
@@ -35,9 +38,11 @@ function Login() {
         >
           Register
         </button>
-        <p className="mt-5 mb-3 text-muted">© Marvel Hero Navegator 2021</p>
-      </S.Form>
-    </S.Container>
+        <p className="mt-5 mb-3 text-muted">
+          © Marvel Hero Navegator 2021
+        </p>
+      </form>
+    </div>
   );
 }
 
