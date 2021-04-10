ec3003b41217078ba1dbffe56667a2676a901d73
deko possas <andrepossaster@gmail.com>
2021-04-10 15:31:13 -0300

    instalei o chakra

--- marvel-hero-navegator-frontend/src/App.jsx
+++ marvel-hero-navegator-frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Provider from './Context/Provider';
 import Login from './pages/Login';
@@ -6,9 +7,11 @@ import Login from './pages/Login';
 function App() {
   return (
     <BrowserRouter>
-      <Provider>
-        <Route exact path="/" component={Login} />
-      </Provider>
+      <ChakraProvider>
+        <Provider>
+          <Route exact path="/" component={Login} />
+        </Provider>
+      </ChakraProvider>
     </BrowserRouter>
   );
 }
