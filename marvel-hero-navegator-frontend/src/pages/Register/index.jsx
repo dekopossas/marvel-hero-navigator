bea9cfb262af726af002df4db4fd0cd01240b17e
deko possas <andrepossaster@gmail.com>
2021-04-10 20:23:03 -0300

    criado a tela de register sem CSS

--- marvel-hero-navegator-frontend/src/pages/Register/index.jsx
+++ marvel-hero-navegator-frontend/src/pages/Register/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Text, Stack } from '@chakra-ui/react';
+import {
+  Text,
+  Stack,
+  Input,
+  Button,
+} from '@chakra-ui/react';
 
 function Register() {
   return (
     <div>
       <Stack spacing={3}>
         <Text fontSize="5xl">Register</Text>
+        <Input placeholder="Email" />
+        <Input placeholder="Passworld" />
+        <Button size="lg" colorScheme="green" mt="24px">
+          Create a free account
+        </Button>
       </Stack>
     </div>
   );
