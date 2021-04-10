ac48a071f762fc00d160b1ca1f72aa2295a97766
deko possas <andrepossaster@gmail.com>
2021-04-10 20:10:38 -0300

    tela de register criada

---
+++ marvel-hero-navegator-frontend/src/pages/Register/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, Stack } from '@chakra-ui/react';
+
+function Register() {
+  return (
+    <div>
+      <Stack spacing={3}>
+        <Text fontSize="5xl">Register</Text>
+      </Stack>
+    </div>
+  );
+}
+
+export default Register;
