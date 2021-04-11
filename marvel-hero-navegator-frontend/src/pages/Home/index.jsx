1bdbad38a9e0f02cb0d9ddb359351945f2ce2777
deko possas <andrepossaster@gmail.com>
2021-04-10 21:01:15 -0300

    troquei o texto para home

--- marvel-hero-navegator-frontend/src/pages/Home/index.jsx
+++ marvel-hero-navegator-frontend/src/pages/Home/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Text } from '@chakra-ui/react';
 
 function Home() {
   return (
     <div>
-      
+      <Text fontSize="5xl">Home</Text>
     </div>
   );
 }
