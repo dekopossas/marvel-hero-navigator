f37c9fe3564a4073354768acd404e9a6de606fdd
deko possas <andrepossaster@gmail.com>
2021-04-10 21:01:36 -0300

    adicionei um input de login irado CSS

--- marvel-hero-navegator-frontend/src/pages/Login/index.jsx
+++ marvel-hero-navegator-frontend/src/pages/Login/index.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { Button, HStack, Stack } from '@chakra-ui/react';
+import {
+  Button,
+  HStack,
+  Stack,
+  Text,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from '@chakra-ui/react';
 import { FaFacebook, FaTwitter } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 function Login() {
+  const [show, setShow] = React.useState(false);
+  const handleClick = () => setShow(!show);
+
   return (
     <div className="text-center">
-      <Stack>
-        <h1 className="h1 mb-3 font-weight-normal">Login</h1>
+      <Stack spacing={3}>
+        <Text fontSize="5xl">Login</Text>
         <label htmlFor="email">
           <input
             className="form-control"
@@ -17,6 +28,14 @@ function Login() {
             placeholder="Email"
           />
         </label>
+        <InputGroup size="md">
+          <Input pr="4.5rem" type={show ? 'text' : 'password'} placeholder="Enter password" />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? 'Hide' : 'Show'}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
         <label htmlFor="password">
           <input
             className="form-control"
@@ -27,22 +46,13 @@ function Login() {
           />
         </label>
         <Link to="/home">
-          <Button
-            colorScheme="green"
-            type="button"
-            data-testid="signin-btn"
-          >
+          <Button colorScheme="green" type="button" data-testid="signin-btn">
             Join
           </Button>
         </Link>
         <br />
         <Link to="/register">
-          <Button
-            colorScheme="blue"
-            to="/register"
-            data-testid="no-account-btn"
-            type="button"
-          >
+          <Button colorScheme="blue" to="/register" data-testid="no-account-btn" type="button">
             Register
           </Button>
         </Link>
