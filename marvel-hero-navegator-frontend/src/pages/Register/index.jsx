81fb332077a45f5963c32e771c5249e38e359892
deko possas <andrepossaster@gmail.com>
2021-04-11 12:04:36 -0300

    CSS do register

--- marvel-hero-navegator-frontend/src/pages/Register/index.jsx
+++ marvel-hero-navegator-frontend/src/pages/Register/index.jsx
@@ -4,15 +4,33 @@ import {
   Stack,
   Input,
   Button,
+  InputGroup,
+  InputRightElement,
 } from '@chakra-ui/react';
 
 function Register() {
+  const [show, setShow] = React.useState(false);
+  const handleClick = () => setShow(!show);
+
   return (
     <div>
-      <Stack spacing={3}>
+      <Stack spacing={3} align="center">
         <Text fontSize="5xl">Register</Text>
-        <Input placeholder="Email" />
-        <Input placeholder="Passworld" />
+        <Input placeholder="Email" width="300px" />
+        <InputGroup size="md" width="300px">
+          <Input
+            pr="4.5rem"
+            type={show ? 'text' : 'password'}
+            placeholder="Enter password"
+            data-testid="password-input"
+            id="password"
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? 'Hide' : 'Show'}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
         <Button size="lg" colorScheme="green" mt="24px">
           Create a free account
         </Button>
