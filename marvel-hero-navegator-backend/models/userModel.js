a0123688e1323e732fd7a28daa4350cf6d33d071
deko possas <andrepossaster@gmail.com>
2021-04-12 08:42:53 -0300

    criei uma requisicao de get all users

--- marvel-hero-navegator-backend/models/userModel.js
+++ marvel-hero-navegator-backend/models/userModel.js
@@ -3,6 +3,10 @@ const connection = require('./connection');
 const createUser = async (name, email, password) => connection()
   .then((db) => db.collection('users').insertOne(name, email, password));
 
+const getAllUsers = async () => connection()
+  .then((db) => db.collection('users').find().toArray());
+
 module.exports = {
-  createUser
+  createUser,
+  getAllUsers,
 };
