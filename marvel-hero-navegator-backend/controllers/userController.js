7ff5b6ef1dcd5928851b99fe5feeeebeea9b1a1c
deko possas <andrepossaster@gmail.com>
2021-04-12 00:38:58 -0300

    troquei pra new router

--- marvel-hero-navegator-backend/controllers/userController.js
+++ marvel-hero-navegator-backend/controllers/userController.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const user = require('./user');
 const userService = require('../services/userService');
 
-const router = Router();
+const router = new Router();
 
 router.post(
   '/register',
