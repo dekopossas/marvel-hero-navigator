8d2075fe5c08500d6308e2d725bc6722399e3162
deko possas <andrepossaster@gmail.com>
2021-04-11 21:02:35 -0300

    criei a validacao do botao join

--- marvel-hero-navegator-frontend/src/pages/Register/index.jsx
+++ marvel-hero-navegator-frontend/src/pages/Register/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Text,
   Stack,
@@ -9,14 +9,46 @@ import {
 } from '@chakra-ui/react';
 
 function Register() {
-  const [show, setShow] = React.useState(false);
+  const [show, setShow] = useState(false);
+  const [email, setEmail] = useState('');
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [password, setPassword] = useState('');
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
+
   const handleClick = () => setShow(!show);
+
+  const handleChangeEmail = (event, value) => {
+    setEmail(value);
+    const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    if (regex.test(value)) {
+      setIsEmailValid(true);
+    } else {
+      setIsEmailValid(false);
+    }
+  };
+
+  const handleChangePassword = (event, value) => {
+    setPassword(value);
+    const PASSOWARD_MIN_LENGTH = 6;
+
+    if (value.length >= PASSOWARD_MIN_LENGTH) {
+      setIsPasswordValid(true);
+    } else {
+      setIsPasswordValid(false);
+    }
+  };
+
+  
 
   return (
     <div>
       <Stack spacing={3} align="center">
         <Text fontSize="5xl">Register</Text>
-        <Input placeholder="Email" width="300px" />
+        <Input
+          placeholder="Email"
+          width="300px"
+          onChange={handleChangeEmail}
+        />
         <InputGroup size="md" width="300px">
           <Input
             pr="4.5rem"
@@ -24,6 +56,7 @@ function Register() {
             placeholder="Enter password"
             data-testid="password-input"
             id="password"
+            onChange={handleChangePassword}
           />
           <InputRightElement width="4.5rem">
             <Button h="1.75rem" size="sm" onClick={handleClick}>
@@ -31,7 +64,12 @@ function Register() {
             </Button>
           </InputRightElement>
         </InputGroup>
-        <Button size="lg" colorScheme="green" mt="24px">
+        <Button
+          size="lg"
+          colorScheme="green"
+          mt="24px"
+          disabled={!(isEmailValid && isPasswordValid)}
+        >
           Create a free account
         </Button>
       </Stack>
