5da33f762d9a4b5e94c2e3e113be4ca0c745eba3
deko possas <andrepossaster@gmail.com>
2021-04-12 00:30:40 -0300

    criei model de create user

---
+++ marvel-hero-navegator-backend/models/userModel.js
@@ -0,0 +1,8 @@
+const connection = require('./connection');
+
+const createUser = async (name, email, password) => connection()
+  .then((db) => db.collection('users').insertOne(name, email, password));
+
+module.exports = {
+  createUser
+};
