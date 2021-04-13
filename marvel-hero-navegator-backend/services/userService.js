689e14b88b61285fcc3cdfede49116b4d0f7934e
deko possas <andrepossaster@gmail.com>
2021-04-13 12:06:22 -0300

    services criados

---
+++ marvel-hero-navegator-backend/services/userService.js
@@ -0,0 +1,19 @@
+const userModel = require('../models/userModel');
+
+const getAllUsers = async () => userModel.getAllUsers();
+
+const getUsersById = async (id) => cuserModel.getUsersById(id);
+
+const createUser = async (data) => userModel.createUser(data);
+
+const updateUser = async (data) => userModel.updateUser(data)
+  
+const deleteUser = async (id) => userModel.deleteUser(id);
+
+module.exports = {
+  getAllUsers,
+  getUsersById,
+  createUser,
+  updateUser,
+  deleteUser,
+};
