36ce96a2445f8791171991314cf03f511048de46
deko possas <andrepossaster@gmail.com>
2021-04-13 12:45:46 -0300

    user controller

--- marvel-hero-navegator-backend/controllers/userController.js
+++ marvel-hero-navegator-backend/controllers/userController.js
@@ -4,5 +4,31 @@ const userService = require('../services/userService');
 
 const userRouter = new Router();
 
+userRouter.get('/', async (_req, res) => {
+  const response = await userService.getAllUsers();
+  res.status(200).json(response);
+});
 
-module.exports = router;
+userRouter.get('/:id', async (req, res) => {
+  const { id } = req.params
+  const response = await userService.getUsersById(id);
+  res.status(200).json(response);
+});
+
+userRouter.post('/', async (req, res) => {
+  const response = await userService.createUser(req.body);
+  res.status(200).json(response);
+});
+
+userRouter.get('/', async (req, res) => {
+  const response = await userService.updateUser(req.body);
+  res.status(200).json(response);
+});
+
+userRouter.get('/', async (req, res) => {
+  const { id } = req.body;
+  const response = await userService.deleteUser(id);
+  res.status(200).json(response);
+});
+
+module.exports = userRouter;
