bcd623f65a41dc3dadf8f413aceceba58f4d05d2
deko possas <andrepossaster@gmail.com>
2021-04-13 12:06:48 -0300

    alterei o parametro equivocado

--- marvel-hero-navegator-backend/models/userModel.js
+++ marvel-hero-navegator-backend/models/userModel.js
@@ -5,13 +5,13 @@ const getAllUsers = async () => connect('users').then((db) => db.find().toArray(
 
 const getUsersById = async (id) => connect('users').then((db) => db.find(ObjectId(id)));
 
-const createUser = async () => connect('users')
+const createUser = async ({userName, email, password}) => connect('users')
   .then((db) => db.insertOne({userName, email, password}))
   .then((result) => ({ _id: result.insertedId, userName, email, password}));
 
 const updateUser = async ({ id, userName, email, password }) => connect('users')
   .then((db) => db.updateOne({ _id: ObjectId(id) }, { $set: { userName, email, password } }));
-  
+
 const deleteUser = async (id) => connect('users')
   .then((db) => db.deleteOne({ _id: ObjectId(id) }));
 
