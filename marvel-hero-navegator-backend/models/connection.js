c6d20824d8957a934a314273205ed46b220bf485
deko possas <andrepossaster@gmail.com>
2021-04-13 11:36:10 -0300

    connection refatorado

--- marvel-hero-navegator-backend/models/connection.js
+++ marvel-hero-navegator-backend/models/connection.js
@@ -5,14 +5,16 @@ require('dotenv').config();
 const DB_NAME = 'MarvelHeroNavegator';
 const MONGO_DB_URL = `${process.env.MONGO_DB_URL}/${DB_NAME}`;
 
-module.exports = () =>
-  MongoClient
-    .connect(MONGO_DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then((conn) => conn.db(DB_NAME))
-    .catch((err) => {
-      console.log(err);
-      process.exit(1);
-    });
+let connection = null;
+
+const getConnection = async(collectionName) => {
+  connection =
+    connection ||
+      (await MongoClient.connect(MONGO_DB_URL, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+  }));
+  return connection.db(DB_NAME).collection(collectionName)
+};
+
+module.exports = getConnection;
