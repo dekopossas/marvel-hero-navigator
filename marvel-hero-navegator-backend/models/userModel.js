d1243c1802131288b07ed98629ce0198ddb185a6
deko possas <andrepossaster@gmail.com>
2021-04-13 15:32:30 -0300

    alterei o find para findOne

--- marvel-hero-navegator-backend/models/userModel.js
+++ marvel-hero-navegator-backend/models/userModel.js
@@ -3,14 +3,17 @@ const connect = require('./connection');
 
 const getAllUsers = async () => connect('users').then((db) => db.find().toArray());
 
-const getUsersById = async (id) => connect('users').then((db) => db.find(ObjectId(id)));
+const getUsersById = async (id) => connect('users').then((db) => db.findOne(ObjectId(id)));
 
 const createUser = async ({userName, email, password}) => connect('users')
   .then((db) => db.insertOne({userName, email, password}))
   .then((result) => ({ _id: result.insertedId, userName, email, password}));
 
-const updateUser = async ({ id, userName, email, password }) => connect('users')
-  .then((db) => db.updateOne({ _id: ObjectId(id) }, { $set: { userName, email, password } }));
+const updateUser = async ({ id, userName, email, password }) => {
+  await connect('users').then((db) =>
+  db.updateOne({ _id: ObjectId(id) }, { $set: { userName, email, password } }));
+  return { _id:id, userName, email, password };
+}
 
 const deleteUser = async (id) => connect('users')
   .then((db) => db.deleteOne({ _id: ObjectId(id) }));
