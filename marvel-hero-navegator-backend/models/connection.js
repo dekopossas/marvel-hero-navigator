9daf4314e4ebcbf907f4815222055d6108f79cba
deko possas <andrepossaster@gmail.com>
2021-04-12 21:09:26 -0300

    connection

--- marvel-hero-navegator-backend/models/connection.js
+++ marvel-hero-navegator-backend/models/connection.js
@@ -1,20 +1,20 @@
 const { MongoClient } = require('mongodb');
 
+require('dotenv').config();
+
 const DBNAME = 'MarvelHeroNavegator';
 const MONGO_DB_URL = `${process.env.MONGO_DB_URL}/${DBNAME}`;
 
-const SUCESS = 1;
-
 let connection = null;
 
-const getConnection = () =>
-  MongoClient.connection(MONGO_DB_URL, {
-    userNewUrlParser: true,
-    useUnifiedTopology: true,
-  }).then((conn) => conn.db(DBNAME))
-    .catch((err) => {
-      console.error(err);
-      process.exit(SUCESS)
-    });
+const getConnection = async (collectionName) =>{
+  connection = 
+    connection || 
+      (await MongoClient.connect(MONGO_DB_URL, {
+        userNewUrlParser: true,
+        useUnifiedTopology: true,
+      }))
+  return connection.db(DBNAME).collection(collectionName);
+};
 
-module.exports = connection;
+module.exports = getConnection;
