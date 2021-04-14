01f7bd46c218cca469c74a8366c47dc3cb04b738
deko possas <andrepossaster@gmail.com>
2021-04-14 16:09:35 -0300

    trabalhando no nav bar

--- marvel-hero-navegator-frontend/src/components/TopBar/index.jsx
+++ marvel-hero-navegator-frontend/src/components/TopBar/index.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
 import {
+  Stack,
   Menu,
   MenuButton,
+  MenuList,
+  MenuItem,
   IconButton,
-  HamburgerIcon,
 } from '@chakra-ui/react';
+import { ImMenu } from 'react-icons/im';
 
 function TopBar() {
   return (
     <div>
-      <Menu>
-        <MenuButton
-          as={IconButton}
-          aria-label="Options"
-          icon={<HamburgerIcon />}
-          variant="outline"
-        />
-      </Menu>
+      <Stack height="80%" borderRadius="md" width="90%" align="start">
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            aria-label="Options"
+            icon={<ImMenu />}
+            variant="outline"
+          />
+          <MenuList>
+            <MenuItem command="⌘T">
+              New Tab
+            </MenuItem>
+            <MenuItem command="⌘N">
+              New Window
+            </MenuItem>
+            <MenuItem command="⌘⇧N">
+              Open Closed Tab
+            </MenuItem>
+            <MenuItem command="⌘O">
+              Open File...
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </Stack>
     </div>
   );
 }
--- marvel-hero-navegator-frontend/src/pages/Home/index.jsx
+++ marvel-hero-navegator-frontend/src/pages/Home/index.jsx
@@ -8,7 +8,7 @@ import TopBar from '../../components/TopBar';
 function Home() {
   return (
     <div>
-      <Stack spacing={3}>
+      <Stack spacing={3} align="center" height="80%" width="90%">
         <h1> home</h1>
         <TopBar />
         <CardHero />
