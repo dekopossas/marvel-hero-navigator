e3f9b08e96b31971a383f74911244744788ed34d
deko possas <andrepossaster@gmail.com>
2021-04-14 13:32:24 -0300

    CONSEGUIU ME FAZER DESISTIR KKK

--- marvel-hero-navegator-frontend/src/pages/Home/index.jsx
+++ marvel-hero-navegator-frontend/src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import CardHero from '../../components/CardHero';
 
 function Home() {
   return (
     <div>
       <h1> home</h1>
+      <CardHero />
     </div>
   );
 }
