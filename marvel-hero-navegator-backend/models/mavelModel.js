1099b116051685425602592a0a8ca56d9bbf58a8
deko possas <andrepossaster@gmail.com>
2021-04-14 16:43:46 -0300

    model de get all characters

---
+++ marvel-hero-navegator-backend/models/mavelModel.js
@@ -0,0 +1,27 @@
+const fetch = require("node-fetch");
+const crypto = require("crypto");
+require('dotenv').config();
+
+const md5 = (texto) => crypto.createHash("md5").update(texto).digest("hex");
+const ts = Date.now();
+const privateKey = process.env.PRIVATE_KEY
+const publicKey = process.env.PUBLIC_KEY
+const hash = md5(ts + privateKey + publicKey);
+
+const getCharacters = async (offset) => {
+  return fetch(
+    `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}&limit=10&offset=${offset}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+    }
+  )
+  .then((response)=>response.json())
+  .then(({data})=> data.results);
+}
+
+module.exports = {
+  getCharacters,
+};
