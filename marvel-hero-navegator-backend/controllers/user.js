92dfd9aa7787542afff5b50dca5fd577b4af2e9a
deko possas <andrepossaster@gmail.com>
2021-04-14 04:28:23 -0300

    tratamento de erro para user controller

--- marvel-hero-navegator-backend/controllers/user.js
+++ marvel-hero-navegator-backend/controllers/user.js
@@ -1,45 +1,52 @@
-const jwt = require('jsonwebtoken');
 const userModel = require('../models/userModel');
+const authJWT = require('../auth/validateToken');
 
-const secret = 'MarvelHeroNavegatorSecret';
-const jwtConfig = {
-  expiresIn: '7d',
-  algorithm: 'HS256',
-};
-
+const UNAUTHORIZED = 401
 const CONFLICT = 409;
 const CREATED = 201;
 const INTERNAL_ERROR = 500;
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.getUsersByEmail(email);
+
+    if (!user || user.password !== password) {
+      return res.status(UNAUTHORIZED).json({ message: 'Usuário não existe ou senha inválida' });
+    }
+
+    const token = authJWT.jwtSign({ data: user }, authJWT.secret, authJWT.jwtConfig);
+
+    return res.status(200).json({ token, user });
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
+};
 
 const register = async (req, res) => {
   try{
     const {name, email, password} = req.body;
     
-    const token = jwt.sign({ data: [email, password] }, secret, jwtConfig);
-    const user = await userModel.getAllUsers(email);
-    if(user) return res.status(CONFLICT).json({ message: 'Email already in use.'});
+    const token = authJWT.jwtSign({ data: [email, password] }, authJWT.secret, authJWT.jwtConfig);
+
+    const user = await userModel.getUsersByEmail(email);
+    if (user && user.email === email) {
+      return res.status(CONFLICT).json({ message: 'Email already in use.' });
+    };
+
+    await userModel.createUser(name, email, password);
     
-    await userService.createUser(name, email, password);
-    
-    const findNewUser = await userService.findByUserEmail(email);
+    const findNewUser = await userModel.getUsersByEmail(email);
     const userObj = { ...findNewUser, token };
     
     return res.status(CREATED).json({ user: userObj });
   } catch (err) {
-    return res.status(INTERNAL_ERROR).json({ message: `quando eu resovler${err}` });
+    return res.status(INTERNAL_ERROR).json({ message: err });
   }
 };
 
-// const login = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-
-//     const usersList = await userService.getAllUsers();
-
-//     const userExist = await usersList.find((user) => user.email === email);
-//   }
-// }
-
 module.exports = {
   register,
+  login,
 };
