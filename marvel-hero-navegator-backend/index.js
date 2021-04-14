b6e185c81b3ad6e564ce8da079dfe6da97795a96
deko possas <andrepossaster@gmail.com>
2021-04-14 04:29:50 -0300

    recriei o app.use com / sem o option

--- marvel-hero-navegator-backend/index.js
+++ marvel-hero-navegator-backend/index.js
@@ -18,6 +18,6 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
-app.use('/users', userRouter);
+app.use('/', userRouter);
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
