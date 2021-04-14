dc43f645817f612b0cdf229bc0ac2368cf5240a6
deko possas <andrepossaster@gmail.com>
2021-04-14 03:46:35 -0300

    terminei tela de login

--- marvel-hero-navegator-frontend/src/pages/Login/index.jsx
+++ marvel-hero-navegator-frontend/src/pages/Login/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Button,
   HStack,
@@ -9,13 +9,49 @@ import {
   InputRightElement,
 } from '@chakra-ui/react';
 import { FaFacebook, FaTwitter } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import api from '../../services/api';
 
 function Login() {
-  const [show, setShow] = React.useState(false);
+  const [show, setShow] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
+
   const handleClickHideShowBtn = () => setShow(!show);
 
-  const handleClickLogin = () => {};
+  const history = useHistory();
+
+  const handleChangeEmail = (event) => {
+    const { value } = event.target;
+    setEmail(value);
+    const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    if (regex.test(value)) {
+      setIsEmailValid(true);
+    } else {
+      setIsEmailValid(false);
+    }
+  };
+
+  const handleChangePassword = (event) => {
+    const { value } = event.target;
+    setPassword(value);
+    const PASSOWARD_MIN_LENGTH = 6;
+
+    if (value.length >= PASSOWARD_MIN_LENGTH) {
+      setIsPasswordValid(true);
+    } else {
+      setIsPasswordValid(false);
+    }
+  };
+
+  const handleClickLogin = async () => {
+    const response = await api.post('users', { email, password });
+    if (response) {
+      history.push('/home');
+    }
+  };
 
   return (
     <div className="text-center">
@@ -28,6 +64,7 @@ function Login() {
           data-testid="email-input"
           placeholder="Email"
           width="300px"
+          onChange={handleChangeEmail}
         />
         <InputGroup size="md" width="300px">
           <Input
@@ -35,6 +72,7 @@ function Login() {
             type={show ? 'text' : 'password'}
             placeholder="Enter password"
             data-testid="password-input"
+            onChange={handleChangePassword}
             id="password"
           />
           <InputRightElement width="4.5rem">
@@ -50,6 +88,7 @@ function Login() {
             type="button"
             data-testid="signin-btn"
             onclick={handleClickLogin}
+            disabled={!(isEmailValid && isPasswordValid)}
           >
             <Link to="/home">Join</Link>
           </Button>
