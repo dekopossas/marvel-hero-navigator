72eac5f1136d0d615a247f6bc8790c1094a6390b
deko possas <andrepossaster@gmail.com>
2021-04-14 13:12:04 -0300

    nao consigo importar a nav bar

--- marvel-hero-navegator-frontend/src/pages/Home/index.jsx
+++ marvel-hero-navegator-frontend/src/pages/Home/index.jsx
@@ -4,10 +4,8 @@ import {
   Box,
   Image,
   Badge,
-  Stack,
   // StarIcon,
 } from '@chakra-ui/react';
-import NavBar from '../../components/NavBar';
 
 function Home() {
   const property = {
@@ -23,57 +21,54 @@ function Home() {
 
   return (
     <div>
-      <Stack spacing={3} align="center">
-        <NavBar />
-        <Text fontSize="5xl">Home</Text>
-        <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-          <Image src={property.imageUrl} alt={property.imageAlt} />
+      <Text fontSize="5xl">Home</Text>
+      <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+        <Image src={property.imageUrl} alt={property.imageAlt} />
 
-          <Box p="6">
-            <Box d="flex" alignItems="baseline">
-              <Badge borderRadius="full" px="2" colorScheme="teal">
-                New
-              </Badge>
-              <Box
-                color="gray.500"
-                fontWeight="semibold"
-                letterSpacing="wide"
-                fontSize="xs"
-                textTransform="uppercase"
-                ml="2"
-              >
-                {property.beds}
-                beds &bull;
-                {property.baths}
-                baths
-              </Box>
+        <Box p="6">
+          <Box d="flex" alignItems="baseline">
+            <Badge borderRadius="full" px="2" colorScheme="teal">
+              New
+            </Badge>
+            <Box
+              color="gray.500"
+              fontWeight="semibold"
+              letterSpacing="wide"
+              fontSize="xs"
+              textTransform="uppercase"
+              ml="2"
+            >
+              {property.beds}
+              beds &bull;
+              {property.baths}
+              baths
             </Box>
-
-            <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated>
-              {property.title}
-            </Box>
-
-            <Box>
-              {property.formattedPrice}
-              <Box as="span" color="gray.600" fontSize="sm">
-                / wk
-              </Box>
-            </Box>
-
-            {/* <Box d="flex" mt="2" alignItems="center">
-              {Array(5)
-                .fill('')
-                .map((_, index) => (
-                  <StarIcon key={index} color={index < property.rating ? 'teal.500' : 'gray.300'} />
-                ))}
-              <Box as="span" ml="2" color="gray.600" fontSize="sm">
-                {property.reviewCount}
-                reviews
-              </Box>
-            </Box> */}
           </Box>
+
+          <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated>
+            {property.title}
+          </Box>
+
+          <Box>
+            {property.formattedPrice}
+            <Box as="span" color="gray.600" fontSize="sm">
+              / wk
+            </Box>
+          </Box>
+
+          {/* <Box d="flex" mt="2" alignItems="center">
+            {Array(5)
+              .fill('')
+              .map((_, index) => (
+                <StarIcon key={index} color={index < property.rating ? 'teal.500' : 'gray.300'} />
+              ))}
+            <Box as="span" ml="2" color="gray.600" fontSize="sm">
+              {property.reviewCount}
+              reviews
+            </Box>
+          </Box> */}
         </Box>
-      </Stack>
+      </Box>
     </div>
   );
 }
